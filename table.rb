4850190b6aa3093f4c113f9564f684f6d6646bb0
Dmitrii Minaev <dminaev@ooyala.com>
2018-12-31 02:25:01 +0400

    user profiles, rules

--- table.rb
+++ table.rb
@@ -28,6 +28,10 @@ today = DateTime.parse(now.to_s)
 week = today.cweek
 prolog = ""
 
+index_erb = ERB.new(File.read('index.html.erb'))
+rules_erb = ERB.new(File.read('rules.html.erb'))
+user_erb = ERB.new(File.read('u.html.erb'))
+
 db = SQLite3::Database.new("2019.db")
 
 prolog += "<center>\n"
@@ -35,18 +39,22 @@ prolog += "<h1>Пролог</h1>\n"
 prolog += "</center>\n"
 prolog += "<div class=\"datagrid\">\n"
 prolog += "<table>\n"
-prolog += "<thead><tr><th>Имя</th><th>Цель2018 (км)</th><th>Результат (км)</th></tr></thead>\n"
+prolog += "<thead><tr><th>Имя</th><th>Команда</th><th>Цель2018 (км)</th><th>Результат (км)</th></tr></thead>\n"
 prolog += "<tbody>\n"
 
-index_erb = ERB.new(File.read('index.html.erb'))
+teams = db.execute("SELECT * FROM teams")
 
 odd = true
-db.execute("SELECT runners.runnerid,runners.runnername,7*runners.goal/365,COALESCE(SUM(log.distance),0) AS dist FROM log JOIN runners ON log.runnerid=runners.runnerid AND date>'#{STARTPROLOG.iso8601}' AND date<'#{ENDPROLOG.iso8601}' GROUP BY log.runnerid ORDER BY dist DESC").each do |r|
-#    log = db.execute("SELECT COALESCE(SUM(distance),0) AS dist FROM log WHERE runnerid=#{r[0]} AND date>'#{STARTPROLOG.iso8601}' AND date<'#{ENDPROLOG.iso8601}'")
+runners = db.execute("SELECT runnerid,runnername,7*goal/365,teamid FROM runners")
+runners.each do |r|
+    r << db.execute("SELECT COALESCE(SUM(distance),0) AS dist FROM log WHERE runnerid=#{r[0]} AND date>'#{STARTPROLOG.iso8601}' AND date<'#{ENDPROLOG.iso8601}'")[0][0]
+end
+runners.sort! { |x,y| y[4] <=> x[4] }
+runners.each do |r|
     if odd then
-        prolog += "<tr><td>#{r[1]}</td><td>#{r[2].round(2)}</td><td>#{r[3].round(2)}</td></tr>\n"
+        prolog += "<tr><td><a href=\"u#{r[0]}.html\">#{r[1]}</a></td><td>#{teams[r[3]-1][1]}</td><td>#{r[2].round(2)}</td><td>#{r[4].round(2)}</td></tr>\n"
     else
-        prolog += "<tr class=\"alt\"><td>#{r[1]}</td><td>#{r[2].round(2)}</td><td>#{r[3].round(2)}</td></tr>\n"
+        prolog += "<tr class=\"alt\"><td><a href=\"u#{r[0]}.html\">#{r[1]}</a></td><td>#{teams[r[3]-1][1]}</td><td>#{r[2].round(2)}</td><td>#{r[4].round(2)}</td></tr>\n"
     end
     odd = !odd
 end
@@ -55,5 +63,22 @@ prolog += "</tbody>\n"
 prolog += "</table></div>\n"
 
 File.open('html/index.html', 'w') { |f| f.write(index_erb.result) }
+File.open('html/rules.html', 'w') { |f| f.write(rules_erb.result) }
 
+data = ""
+runners = db.execute("SELECT * FROM runners") do |r|
+    data = ""
+    data += "<center>\n"
+    data += "<h1>Карточка участника</h1>\n"
+    data += "</center>\n"
+    data += "<div class=\"datagrid\">\n"
+    data += "<table>\n"
+    data += "<tbody>\n"
+    data += "<tr><td><b>Имя</b></td><td>#{r[1]}</td></tr>"
+    data += "<tr><td><b>Команда</b></td><td>#{teams[r[2]-1][1]}</td></tr>"
+    data += "<tr><td><b>Недельный план</b></td><td>#{(7*r[3]/365).round(2)}</td></tr>"
+    data += "</tbody>\n"
+    data += "</table>\n"
 
+    File.open("html/u#{r[0]}.html", 'w') { |f| f.write(user_erb.result) }
+end
