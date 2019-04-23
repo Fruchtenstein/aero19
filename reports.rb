220c02ea60237ea0156a4730c4613729f97089f2
Dimitri Minaev <dmitry.minaev@magenta-technology.com>
2019-04-23 14:59:00 +0400

    include slowest runs to reports

--- reports.rb
+++ reports.rb
@@ -17,12 +17,22 @@ puts r
 r = `litecli 2019.db -te "select runnername,date,distance,log.runnerid from log,runners where date>'#{bow}' and date<'#{eow}' and log.runnerid=runners.runnerid and sex=0 order by distance desc limit 5"` 
 puts r
 puts "самая быстрая пробежка"
-r = `litecli 2019.db -te "select runnername,date,time/distance s,distance, log.runnerid from log,runners where date>'#{bow}' and date<'#{eow}' and log.runnerid=runners.runnerid order by s limit 5"`
+r = `litecli 2019.db -te "select runnername,date,strftime('%M:%S',time/distance,'unixepoch') pace,distance, log.runnerid from log,runners where date>'#{bow}' and date<'#{eow}' and log.runnerid=runners.runnerid order by pace limit 5"`
 puts r
-r = `litecli 2019.db -te "select runnername,date,time/distance s,distance, log.runnerid from log,runners where date>'#{bow}' and date<'#{eow}' and log.runnerid=runners.runnerid and sex=0 order by s limit 5"`
+r = `litecli 2019.db -te "select runnername,date,strftime('%M:%S',time/distance,'unixepoch') pace,distance, log.runnerid from log,runners where date>'#{bow}' and date<'#{eow}' and log.runnerid=runners.runnerid and sex=0 order by pace limit 5"`
+puts r
+puts "самая медленная пробежка"
+r = `litecli 2019.db -te "select runnername,date,strftime('%M:%S',time/distance,'unixepoch') pace,distance, log.runnerid from log,runners where date>'#{bow}' and date<'#{eow}' and log.runnerid=runners.runnerid order by pace DESC limit 10"`
+puts r
+r = `litecli 2019.db -te "select runnername,date,strftime('%M:%S',time/distance,'unixepoch') pace,distance, log.runnerid from log,runners where date>'#{bow}' and date<'#{eow}' and log.runnerid=runners.runnerid and sex=0 order by pace DESC limit 5"`
 puts r
 puts "самая быстрая неделя"
-r = `litecli 2019.db -te "SELECT l.runnerid, runnername, t/d pace, strftime('%M:%S',t/d,'unixepoch'), teamname FROM (SELECT runnerid, SUM(time) t, SUM(distance) d FROM log WHERE date>'#{bow}' AND date<'#{eow}' AND time>0 GROUP BY runnerid) l, runners, teams WHERE runners.runnerid=l.runnerid AND teams.teamid=runners.teamid ORDER BY pace LIMIT 5"`
+r = `litecli 2019.db -te "SELECT l.runnerid, runnername, strftime('%M:%S',t/d,'unixepoch') pace, teamname FROM (SELECT runnerid, SUM(time) t, SUM(distance) d FROM log WHERE date>'#{bow}' AND date<'#{eow}' AND time>0 GROUP BY runnerid) l, runners, teams WHERE runners.runnerid=l.runnerid AND teams.teamid=runners.teamid ORDER BY pace LIMIT 5"`
 puts r
-r = `litecli 2019.db -te "SELECT l.runnerid, runnername, t/d pace, strftime('%M:%S',t/d,'unixepoch'), teamname FROM (SELECT runnerid, SUM(time) t, SUM(distance) d FROM log WHERE date>'#{bow}' AND date<'#{eow}' AND time>0 GROUP BY runnerid) l, runners, teams WHERE runners.runnerid=l.runnerid AND sex=0 AND teams.teamid=runners.teamid ORDER BY pace LIMIT 5"`
+r = `litecli 2019.db -te "SELECT l.runnerid, runnername, strftime('%M:%S',t/d,'unixepoch') pace, teamname FROM (SELECT runnerid, SUM(time) t, SUM(distance) d FROM log WHERE date>'#{bow}' AND date<'#{eow}' AND time>0 GROUP BY runnerid) l, runners, teams WHERE runners.runnerid=l.runnerid AND sex=0 AND teams.teamid=runners.teamid ORDER BY pace LIMIT 5"`
+puts r
+puts "самая медленная неделя"
+r = `litecli 2019.db -te "SELECT l.runnerid, runnername, strftime('%M:%S',t/d,'unixepoch') pace, teamname FROM (SELECT runnerid, SUM(time) t, SUM(distance) d FROM log WHERE date>'#{bow}' AND date<'#{eow}' AND time>0 GROUP BY runnerid) l, runners, teams WHERE runners.runnerid=l.runnerid AND teams.teamid=runners.teamid ORDER BY pace DESC LIMIT 5"`
+puts r
+r = `litecli 2019.db -te "SELECT l.runnerid, runnername, strftime('%M:%S',t/d,'unixepoch') pace, teamname FROM (SELECT runnerid, SUM(time) t, SUM(distance) d FROM log WHERE date>'#{bow}' AND date<'#{eow}' AND time>0 GROUP BY runnerid) l, runners, teams WHERE runners.runnerid=l.runnerid AND sex=0 AND teams.teamid=runners.teamid ORDER BY pace DESC LIMIT 5"`
 puts r
