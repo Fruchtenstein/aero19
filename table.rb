8e70738b889c9d04b76871ac59aa0c609cf4a6c6
Dmitrii Minaev <dminaev@ooyala.com>
2019-05-03 18:21:21 +0400

    add columns to users, users2, users3 pages

--- table.rb
+++ table.rb
@@ -264,14 +264,14 @@ data += "</center>\n"
 data += "<div class=\"datagrid\">\n"
 data += "<table>\n"
 data += "<tbody>\n"
-data += "<thead><tr><th>Имя</th><th>Объемы 2019 (км)</th><th>Команда</th></tr></thead>\n"
+data += "<thead><tr><th>Имя</th><th>Объемы 2019 (км)</th><th>Объемы 2019 (%)</th><th>Объемы 2018 (км)</th><th>Команда</th></tr></thead>\n"
 odd = true
-db.execute("SELECT runnerid, runnername, teamname, (SELECT COALESCE(SUM(distance),0) FROM log WHERE runnerid=runners.runnerid GROUP BY runnerid) d FROM runners JOIN teams USING (teamid) ORDER BY d DESC") do |r|
+db.execute("SELECT runnerid, runnername, teamname, (SELECT COALESCE(SUM(distance),0) FROM log WHERE runnerid=r.runnerid GROUP BY runnerid) d, (SELECT goal FROM runners WHERE runnerid=r.runnerid) g, 100*(SELECT COALESCE(SUM(distance),0) FROM log WHERE runnerid=r.runnerid GROUP BY runnerid)/(SELECT goal FROM runners WHERE runnerid=r.runnerid) FROM runners r JOIN teams USING (teamid) ORDER BY d DESC") do |r|
   note = db.execute("SELECT title FROM titles WHERE runnerid=#{r[0]}").join("<br />")
   if odd
-    data += "<tr><td><a href=\"u#{r[0]}.html\">#{r[1]}</a></td><td>#{r[3].round(2)}</td><td>#{r[2]}</td></tr>\n"
+    data += "<tr><td><a href=\"u#{r[0]}.html\">#{r[1]}</a></td><td>#{r[3].round(2)}</td><td>#{r[5].round(2)}</td><td>#{r[4].round(2)}</td><td>#{r[2]}</td></tr>\n"
   else
-    data += "<tr class=\"alt\"><td><a href=\"u#{r[0]}.html\">#{r[1]}</a></td><td>#{r[3].round(2)}</td><td>#{r[2]}</td></tr>\n"
+    data += "<tr class=\"alt\"><td><a href=\"u#{r[0]}.html\">#{r[1]}</a></td><td>#{r[3].round(2)}</td><td>#{r[5].round(2)}</td><td>#{r[4].round(2)}</td><td>#{r[2]}</td></tr>\n"
   end
   odd = !odd
 end
@@ -289,14 +289,14 @@ data += "</center>\n"
 data += "<div class=\"datagrid\">\n"
 data += "<table>\n"
 data += "<tbody>\n"
-data += "<thead><tr><th>Имя</th><th>Объемы 2019 (%)</th><th>Команда</th></tr></thead>\n"
+data += "<thead><tr><th>Имя</th><th>Объемы 2019 (%)</th><th>Объемы 2019 (км)</th><th>Объемы 2018 (км)</th><th>Команда</th></tr></thead>\n"
 odd = true
-db.execute("SELECT runnerid, runnername, teamname, (SELECT 100*COALESCE(SUM(distance),0)/(SELECT goal FROM runners WHERE runnerid=log.runnerid) FROM log WHERE runnerid=runners.runnerid GROUP BY runnerid) d FROM runners JOIN teams USING (teamid) ORDER BY d DESC") do |r|
+db.execute("SELECT runnerid, runnername, teamname, (SELECT COALESCE(SUM(distance),0) FROM log WHERE runnerid=r.runnerid GROUP BY runnerid) d, (SELECT goal FROM runners WHERE runnerid=r.runnerid) g, 100*(SELECT COALESCE(SUM(distance),0) FROM log WHERE runnerid=r.runnerid GROUP BY runnerid)/(SELECT goal FROM runners WHERE runnerid=r.runnerid) p FROM runners r JOIN teams USING (teamid) ORDER BY p DESC") do |r|
   note = db.execute("SELECT title FROM titles WHERE runnerid=#{r[0]}").join("<br />")
   if odd
-    data += "<tr><td><a href=\"u#{r[0]}.html\">#{r[1]}</a></td><td>#{r[3].round(2)}</td><td>#{r[2]}</td></tr>\n"
+    data += "<tr><td><a href=\"u#{r[0]}.html\">#{r[1]}</a></td><td>#{r[5].round(2)}</td><td>#{r[3].round(2)}</td><td>#{r[4].round(2)}</td><td>#{r[2]}</td></tr>\n"
   else
-    data += "<tr class=\"alt\"><td><a href=\"u#{r[0]}.html\">#{r[1]}</a></td><td>#{r[3].round(2)}</td><td>#{r[2]}</td></tr>\n"
+    data += "<tr class=\"alt\"><td><a href=\"u#{r[0]}.html\">#{r[1]}</a></td><td>#{r[5].round(2)}</td><td>#{r[3].round(2)}</td><td>#{r[4].round(2)}</td><td>#{r[2]}</td></tr>\n"
   end
   odd = !odd
 end
